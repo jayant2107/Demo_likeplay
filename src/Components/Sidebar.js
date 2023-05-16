a51fbe632de9e294eb024c7219acdb63959e0eca
Karan Middha <sfs.karan99@gmail.com>
2023-05-16 19:30:32 +0530

    sidebar

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,168 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import {sidebarimage} from '../Utils/Images';
+import { Admirers, Admiring, FAQ, Home, Matches, Menu, Messages, Notification, Setting, Subscription, Testimonial } from '../Utils/Logo';
+
+
+
+
+
+export default function Sidebar() {
+    return (
+        <>
+            <Sidebarwrapper>
+                <Mainlogo>
+                    <img src={sidebarimage}
+                        alt=''
+                        style={
+                            {color: 'red'}
+                        }/>
+
+
+                </Mainlogo>
+                <Slinkcontainer>
+                    <Slogo>
+                    <Home/>
+                        
+                    </Slogo>
+                    <Slabel>
+                        <p>Home</p>
+                    </Slabel>
+                </Slinkcontainer>
+                <Slinkcontainer>
+                    <Slogo>
+                    <Admiring/>
+                        
+                    </Slogo>
+                    <Slabel>
+                        <p>Admiring</p>
+                    </Slabel>
+                </Slinkcontainer>
+                <Slinkcontainer>
+                    <Slogo>
+                    <Admirers/>
+                       
+                    </Slogo>
+                    <Slabel>
+                        <p>Admirers</p>
+                    </Slabel>
+                </Slinkcontainer>
+                <Slinkcontainer>
+                    <Slogo>
+                    <Messages color='#7B7F91'/>
+                        
+                    </Slogo>
+                    <Slabel>
+                        <p>Messages</p>
+                    </Slabel>
+                </Slinkcontainer>
+                <Slinkcontainer>
+                    <Slogo>
+                    <Notification/>
+                      
+                    </Slogo>
+                    <Slabel>
+                        <p>Notification</p>
+                    </Slabel>
+                </Slinkcontainer>
+                <Slinkcontainer>
+                    <Slogo>
+                    <Matches/>
+                       
+                    </Slogo>
+                    <Slabel>
+                        <p>Matches</p>
+                    </Slabel>
+                </Slinkcontainer>
+                <Slinkcontainer>
+                    <Slogo>
+                    <Setting/>
+                       
+                    </Slogo>
+                    <Slabel>
+                        <p>Setting</p>
+                    </Slabel>
+                </Slinkcontainer>
+                <Slinkcontainer>
+                    <Slogo>
+                    <Subscription/>
+                      
+                    </Slogo>
+                    <Slabel>
+                        <p>Subscription</p>
+                    </Slabel>
+                </Slinkcontainer>
+                <Slinkcontainer>
+                    <Slogo>
+                    <FAQ/>
+                        
+                    </Slogo>
+                    <Slabel>
+                        <p>FAQs</p>
+                    </Slabel>
+                </Slinkcontainer>
+                <Slinkcontainer>
+                    <Slogo>
+                    <Testimonial/>
+                        
+                    </Slogo>
+                    <Slabel>
+                        <p>Testimonial</p>
+                    </Slabel>
+                </Slinkcontainer>
+                <Slinkcontainer>
+                    <Slogo>
+                 <Menu color='black'/>
+                    </Slogo>
+                    <Slabel>
+                        <p style={{color:'black'}}>Menu</p>
+                    </Slabel>
+                </Slinkcontainer>
+            </Sidebarwrapper>
+
+        </>
+    )
+}
+const Sidebarwrapper = styled.div `
+width:300px;
+height:100vh;
+position:fixed;
+background-color:white;
+border-right:1px solid #E2E2E2
+`
+const Mainlogo = styled.div `
+display:flex;
+align-items:center
+width:100%;
+justify-content:center;
+padding:20px;
+img{
+    height:100px;
+    width:110px;
+}
+`
+const Slinkcontainer = styled.div `
+display:flex;
+align-items:center;
+gap:20px;
+padding:0px 80px;
+&:hover{
+    p{
+        color:brown;
+    }
+}
+
+`
+
+const Slogo = styled.div `
+&:hover{
+    color:"red"
+}
+`
+const Slabel = styled.div `
+p{
+    font-size:16px;
+    color:#7B7F91;
+}
+`
