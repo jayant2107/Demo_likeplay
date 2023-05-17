6bb91ad91cb0337d8251be6d1996e70bf4c1ee7a
Paras Grover <parasgrover932@gmail.com>
2023-05-17 10:15:24 +0530

    paras

---
+++ src/Data/PublicfeatureData.jsx
@@ -0,0 +1,77 @@
+// logo
+import { category1,category2,category3,category4,category5,category6,category7,category8,category9,category10,category11 } from "../Utils/images/Publichomeimg";
+const featuresData = [
+  {
+    logo: category1,
+    heading: "Take the Floor, Pick the Mic",
+    paragraph:
+      "Come and share the fun! Show Up and show-off join the conversation, post your picture regularly and get more Likes!",
+  },
+  {
+    logo: category2,
+    heading: "Hangout",
+    paragraph:
+      "We organize Hangouts online for a more interactive space to get together and enjoy an evening of fun!",
+  },
+  {
+    logo: category3,
+    heading: "Translation",
+    paragraph:
+      "Connect with just about anyone. Don’t miss out on your chance and easily talk to anybody from any country.",
+  },
+  {
+    logo: category4,
+    heading: "Profile Verification",
+    paragraph:
+      "Have that peace of mind when chatting with online singles. We protect you from fraudsters and the like!",
+  },
+  {
+    logo: category5,
+    heading: "Tags",
+    paragraph:
+      "Have you posted something on The Floor? or did you Pick the Mic? Let your matches know, so they can amplify it, and have loads of cheers.",
+  },
+  {
+    logo: category6,
+    heading: "Chat",
+    paragraph:
+      "Get chatting with the one you Admire, that is why you are here, don't be shy, reach out and start something sweet, and loving.",
+  },
+  {
+    logo: category7,
+    heading: "Favourite",
+    paragraph:
+      "Like what another member always has to say? make them your Favorite and be notified once they Take the Floor, or Pick the Mic.",
+  },
+  {
+    logo: category8,
+    heading: "Super User",
+    paragraph: "Super User with up to 21 comments per week",
+  },
+  {
+    logo: category9,
+    heading: "Referral",
+    paragraph:
+      "Enjoy a FREE Standard subscription when you introduce 5 friends who use your referral code and sign up. The more, the merrier.",
+  },
+  {
+    logo: category10,
+    heading: "Video call",
+    paragraph:
+      "Get a higher level subscription to enable video-calling with your matches, and add another layer of fun.",
+  },
+  {
+    logo: category11,
+    heading: "Voice Notes",
+    paragraph:
+      "Get our Gold Premium package and add audio voice to allow you leave love messages to the one you admire once they Admire back.",
+  },
+  {
+    logo: category1,
+    heading: "Africawide",
+    paragraph:
+      "Our platform is on other African countries. please sign up and complete your profile, to enjoy love with brothers and sisters in other African countries",
+  },
+];
+
+export default featuresData;
