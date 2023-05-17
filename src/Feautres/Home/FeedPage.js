a66632e9ecd1fe209f864ec6700c9bf3083e022a
Ravinder1214 <131768212+Ravinder1214@users.noreply.github.com>
2023-05-17 11:03:51 +0530

    pullmain

--- src/Feautres/Home/FeedPage.js
+++ src/Feautres/Home/FeedPage.js
@@ -5,13 +5,20 @@ import styled from "styled-components";
 import userProfileImg from "../../Assets/Images/userProfileFeed.png";
 import HeartTickImg from "../../Assets/Images/heartTickFeed.png";
 import ThreeDotsImg from "../../Assets/Images/OptionsDotsFeed.png";
-import UserPostImg from "../../Assets/Images/userPost.png"
-import DownArrowImg from '../../Assets/Images/downArrow.png'
+import UserPostImg from "../../Assets/Images/userPost.png";
+import DownArrowImg from "../../Assets/Images/downArrow.png";
 //IconsFunctions
-import { CommentFeedIcon, HeartFeedIcon, LikeFeedIcon, StarFeedIcon, TagFeedIcon } from "../../Utils/HomeIconsFun";
- 
+import {
+  CommentFeedIcon,
+  HeartFeedIcon,
+  LikeFeedIcon,
+  StarFeedIcon,
+  TagFeedIcon,
+} from "../../Utils/HomeIconsFun";
+
 const FeedPage = () => {
   return (
+    <>
     <UserPostCss>
       <div className="userProfile">
         <div className="profileImg">
@@ -42,31 +49,40 @@ const FeedPage = () => {
       <div className="commentLike">
         <div className="likeDiv">
           <div>
-          <LikeFeedIcon />
-          <span>18</span>
+            <LikeFeedIcon />
+            <span>18</span>
           </div>
           <div>
-          <HeartFeedIcon/>
-          <span>18</span>
+            <HeartFeedIcon/>
+            <span>18</span>
           </div>
           <div>
-          <StarFeedIcon/>
-          <span>18</span>
+            <StarFeedIcon />
+            <span>18</span>
           </div>
         </div>
         <div className="commentDiv">
-           <span><CommentFeedIcon/></span><span> 18 Comments</span>
+          <span>
+            <CommentFeedIcon />
+          </span>
+          <span> 18 Comments</span>
         </div>
         <div className="commentDiv">
-          <span><TagFeedIcon/></span><span>5 tags</span><span className="arrow"><img src={DownArrowImg} alt='DownArror'/></span>
+          <span>
+            <TagFeedIcon />
+          </span>
+          <span>5 tags</span>
+          <span className="arrow">
+            <img src={DownArrowImg} alt="DownArror" />
+          </span>
         </div>
       </div>
       <div className="commentInput">
-        <input type='text' placeholder="Add a Comment...."/>
-        <button>Post</button>
+        <input className="input" type="text" placeholder="Add a Comment...." />
+        <button className="inputButton">Post</button>
       </div>
-      <h2>hello</h2>
     </UserPostCss>
+    </>
   );
 };
 
@@ -77,7 +93,7 @@ export const UserPostCss = styled.div`
   //   margin: auto;
   height: auto;
   padding: 1rem;
-  border: 1px solid gray;
+  border-bottom: 1px solid #e2e2e2;
 
   .userProfile {
     display: grid;
@@ -87,13 +103,13 @@ export const UserPostCss = styled.div`
   }
   .profileImg {
     border: 1px solid transparent;
-    border-radius:2rem;
-    height : 3.7rem;
-    width :100%;
-    overflow : hidden;
-    img{
-        width : 100%;
-        height : 100%;
+    border-radius: 2rem;
+    height: 3.7rem;
+    width: 100%;
+    overflow: hidden;
+    img {
+      width: 100%;
+      height: 100%;
     }
   }
   .ProfileInfo {
@@ -141,48 +157,65 @@ export const UserPostCss = styled.div`
     }
   }
 
-.commentLike{
-    display : grid;
-    grid-template-columns : 32% 32% 32%;
+  .commentLike {
+    display: grid;
+    grid-template-columns: 32% 32% 32%;
     justify-content: space-between;
-    height : 4rem;
-    div{
-      background-color : #f7f7f7;
-      border-radius : 0.8rem;
+    height: 4rem;
+    div {
+      background-color: #f7f7f7;
+      border-radius: 0.8rem;
+      font-size: 0.9rem;
+      color: #7b7f91;
+      // #c5c7ce
+    }
   }
-}
 
-.likeDiv{
-  display : flex;
-  justify-content : space-around;
-  align-items : center;
-  div{
-    display : flex;
-    justify-content : space-around;
-    align-items : center;
-  }
-}
-.commentDiv{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  span{
+  .likeDiv {
     display: flex;
-    margin : 0 0.2rem;
+    justify-content: space-around;
+    align-items: center;
+    div {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      span{
+        margin-left:0.4rem;
+      }
+    }
   }
-  .arrow{
-    margin : 0 0.8rem;
+
+  .commentDiv {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    span {
+      display: flex;
+      margin-left: 0.4rem;
+    }
+    .arrow {
+      margin-left: 1.5rem;
+    }
   }
-}
 
-.commentInput{
-  position : relative;
-
-  input{
-    width : 100%;
-    height : auto;
-    font-size : 0.9rem;
+  .commentInput {
+    display:grid;
+    grid-template-columns : auto 71px;
+    margin :1rem 0;
+  }
+  .input {
+    height: 2rem;
+    font-size: 0.9rem;
+    border: 1px solid white;
+    font-size : 1rem;
+  }
+  .inputButton{
+    height: 36px;
+    width : 70px;
     border : 1px solid white;
+    background-color : white;
+    cursor : pointer;
+    font-weight : 500;
+    font-size : 1rem;
   }
-}
 `;
