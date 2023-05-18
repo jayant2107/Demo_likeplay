606c26ff20bae2213edd1670ff3f637b34e829fa
Ravinder1214 <131768212+Ravinder1214@users.noreply.github.com>
2023-05-18 10:57:16 +0530

    update to main

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ import FeedShots from "./Feautres/Home/FeedShots";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <Publichome /> */}
-    <FeedPage/>
+    {/* <FeedPage/> */}
     {/* <FeedAboutMe/> */}
     {/* <FeedShots/> */}
   </React.StrictMode>
