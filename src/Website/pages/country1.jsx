0b9453d8fc9a25c838b49b8130fe2bbe73261841
Jayant <sfs.jayant21@gmail.com>
2023-05-18 14:39:21 +0530

    delete useless files

--- src/Website/pages/country1.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function country1() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
--- src/Website/pages/country3.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function country3() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
