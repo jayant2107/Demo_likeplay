942882e74f44bed8adda5e9569d0a3ea3e3eaeb5
pankaj <sfs.pankaj2022@gmail.com>
2023-05-19 19:45:16 +0530

    Added toast

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
 import "./App.css";
 import React from "react";
 import Routing from "./Routes/Routing";
-
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Routing />
     </>
   );
