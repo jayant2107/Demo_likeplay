fe410d2bb267fd4da7ee3523fd7f2e886720e46c
Paras Grover <parasgrover932@gmail.com>
2023-05-19 18:18:07 +0530

    mereging code

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
 import rootReducer from "../Redux/CombineReducer";
 const persistConfig = {
   key: "root",
@@ -9,6 +10,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 let store = configureStore({
   reducer: persistedReducer,
+  middleware: [thunk],
 });
 export const persistor = persistStore(store);
 export default store;
