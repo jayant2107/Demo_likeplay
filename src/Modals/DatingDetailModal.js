affcdaa6fa7cbfc28255d222eb2099633b1ea16e
arpita-garg <107560329+arpita-garg@users.noreply.github.com>
2023-05-22 18:36:49 +0530

    Co-authored-by: Manav Gupta <manav98@users.noreply.github.com>

--- src/Modals/DatingDetailModal.js
+++ src/Modals/DatingDetailModal.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { likep } from "../Utils/images/Modalsimg";
+import DatingData from "./ModalData/DatingDetailData";
 
 export default function DatingDetailModal() {
   return (
@@ -8,18 +9,42 @@ export default function DatingDetailModal() {
         <div className="modal">
           <div className="modal-container">
             <div className="modal-content">
-              <div className="head">
+              {DatingData.map((data) => {
+                console.log("datahead", data.head);
+                return (
+                  <>
+                    {/* <div className="head">
+                      <img src={likep} alt="likep" id="likep" />
+                      <h5>{data.head}</h5>
+                    </div>
+                    <p>{data.para}.</p> */}
+                    <div className="list-section">
+                      <div className="list-content">
+                        <img src={likep} alt="likep" id="likep" />
+                        <div className="line">
+                          <div className="head">{data.head}</div>
+                          <div className="para">{data.para}</div>
+                          <div className="para1">{data.para1}</div>
+                          <div className="para2">{data.para2}</div>
+                          <div className="para3">{data.para3}</div>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+              {/* <div className="head">
               <img src={likep} alt="likep" id="likep" />
                 <h5>Online Dating Tips:- Dating Safety Tips</h5>
               </div>
               <p>We want to make sure you stay safe during your online
                         dating experience with Seeking. Keep these tips in mind
                         when you are talking/texting with someone or setting up
-                        a Meet & Greet.</p>
+                        a Meet & Greet.</p> */}
               {/* <div className="content-container">
                 {/* MODAL LIST-SECTION STARTS */}
 
-                {/* <div className="list-section">
+              {/* <div className="list-section">
                   <div className="list-content">
                       <img src={likep} alt="likep" id="likep" />
                     <div className="line">
@@ -36,7 +61,7 @@ export default function DatingDetailModal() {
                   </div>
                 </div>
  */}
-                {/* MODAL LIST-SECTION ENDS */}
+              {/* MODAL LIST-SECTION ENDS */}
               {/* </div> */}
             </div>
           </div>
@@ -51,7 +76,7 @@ export const StyledDatingDetails = styled.div`
     width: 100%;
     height: 100vh;
     background-color: rgb(0, 0, 0, 0.5);
-    position: fixed;
+    position: relative;
     top: 0;
     left: 0;
     bottom: 0;
@@ -60,22 +85,22 @@ export const StyledDatingDetails = styled.div`
   }
 
   .modal-content {
-    width: 100%;
+    width: 97%;
     border: 1px solid transparent;
     border-radius: 12px;
     background-color: white;
     box-shadow: rgba(0, 0, 0, 0.5) 0px 1px 20px 0px;
     margin: auto;
-    position: fixed;
-    top: 12%;
+    position: absolute;
+    top: 95%;
     left: 50%;
     padding: 1rem 0 1rem 8rem;
 
     transform: translate(-50%, -50%);
-    p{
-      width: 80%;
+    p {
+      width: 73%;
       margin-left: 25px;
-      font-size: 16 px;
+      font-size: 14px;
     }
   }
 
@@ -84,37 +109,37 @@ export const StyledDatingDetails = styled.div`
   }
 
   .list-section {
-    width: 80%;
-    margin: auto;
-}
+    width: 91%;
+  }
 
   .list-content {
     display: flex;
-    width: 85%;
-    justify-content: space-around;
+    width: 88%;
+  }
+
+  .line {
+    margin-left: 3px;
   }
 
   img#likep {
     width: 14px;
     height: 12px;
-}
+    margin-top: 3px;
+  }
 
   .head {
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  h5{
-    font-size: 16px;
+    display: flex;
+    align-items: center;
+    /* margin-bottom: 3px; */
+    font-size: 15px;
+    font-weight: 600;
   }
-  
-}
 
-
-.para {
-    font-size: 13px;
+  .para, .para1, .para2, .para3{
+    font-size: 15px;
     font-weight: 400;
-    width: 92%;
     padding: 2px;
-
-}
+    width: 98%;
+    color: #484848;
+  }
 `;
