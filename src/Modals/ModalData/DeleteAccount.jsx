af56bf3700a5ff003fa2b8ca42767d8aef90ec19
Param <pramsinghmalout@gmail.com>
2023-05-23 17:48:24 +0530

    apply the clicking modals

---
+++ src/Modals/ModalData/DeleteAccount.jsx
@@ -0,0 +1,14 @@
+const DeleteAccount = {
+  name: "Delete Account",
+  para: "Are you sure you want to Delete this Account?",
+};
+
+export default DeleteAccount;
+export const CancelSubscriptiondata = {
+  name: "Cancel Subscription",
+  para: "Are you Sure You want to Cancel the Subscription",
+};
+export const BlockUserdata = {
+  name: "Blocked User",
+  para: "Are you Sure You want to Block the User",
+};
