d0f948c8b225fb5662cd1b66410074656c4f76a3
aks122 <akshaymahajan931@gmail.com>
2023-05-23 09:24:55 +0530

    admin

--- src/index.js
+++ src/index.js
@@ -5,31 +5,17 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import store, { persistor } from "./Store/Store";
-<<<<<<< HEAD
 import Subscription from "./Feautres/Subscription/Subscriptions";
-import Testimonial from "./Feautres/Testimonial/Testimonial";
-=======
-import "react-chat-elements/dist/main.css"
->>>>>>> 10f3f407daa90074445d9fad290562169dcd4d1b
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 
-<<<<<<< HEAD
-  // <Provider store={store}>
-  //   <PersistGate loading={null} persistor={persistor}>
-  //     <App />
-  //   </PersistGate>
-  // </Provider>
-  // <Subscription/>
-  <Testimonial/>
-=======
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>
->>>>>>> 10f3f407daa90074445d9fad290562169dcd4d1b
+  // <Subscription/>
 );
 
 reportWebVitals();
